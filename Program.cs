0b733c82973655d3a7b699fe190a51c68e1afbe7
ahmadmm01 <skyahmad74@gmail.com>
2022-03-16 15:18:42 +0700

    Create Create_Table method to creat table in SQL Server

--- Program.cs
+++ Program.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Create_Table
 {
     class Program
     {
+        public void CreateTable() {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source= MADD\\AHMADMM; database=ProdiTI; User ID= sa; Password=root");
+                con.Open();
+
+                SqlCommand cm = new SqlCommand("create table Mahasiswa_Coba (NIM char(12) not null primary key, Nama varchar(58), Alamat varchar(255), Jenis_Kelamin char(1))", con);
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Tabel sukses dibuat!");
+                Console.ReadKey();
+            }
+            catch (Exception e) {
+                Console.WriteLine("Oops, sepertinya ada yang salah. " + e);
+                Console.ReadKey();
+            } finally
+            {
+                con.Close();
+            }
+        }
         static void Main(string[] args)
         {
+            new Program().CreateTable();
         }
     }
 }
